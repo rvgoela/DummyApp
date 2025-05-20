9cc1e99e99b033853f3985da0d303f288bc4f81e
Ranveer singh <ranveer.rathore@novelveritas.com>
2025-05-20 19:07:19 +0530

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Hive Todo',
+      title: 'Todo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeScreen(),
     );
